03234432d5efa7c2f1ff1f4854df2f6c4d9d4943
imaneamara <heavenstyles.1d.ia@gmail.com>
2022-04-26 00:10:55 +0200

    test js

--- pdf.js
+++ pdf.js
@@ -1,1 +1,10 @@
+window.onload = function(){
+    document.getElementById("download")
+    .addEventListener("click",()=>{
+        const curriculumvitae = this.document.getElementById("curriculumvitae");
+        console.log(curriculumvitae);
+        console.log(window);
+        html2pdf().from(invoice).save();
+    })
+}
 
